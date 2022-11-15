ef3e28f22d033ce155e0110651592669eee574c9
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-11-15 17:35:20 +0100

    render a pagina della card tramite chiamata API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <ul>
+        <li v-for="value in cityWeather" :key="value">
+
+          <div>
+            <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
+              tag="article" style="max-width: 20rem;" class="mb-2">
+              <b-card-text>
+                {{ value.main }}
+              </b-card-text>
+
+
+            </b-card>
+          </div>
+
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    return {
+      city: "Milan",
+      APIkey: "95b65f89ece539353a36f3e48cfc0c86",
+      cityWeather: [],
+    }
+  },
+  created() {
+    this.getWeather()
+  },
+  methods: {
+    getWeather() {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.APIkey}&lang=it&units=metric`)
+        .then(res => {
+          console.log(res.data)
+          this.cityWeather.push(res.data)
+        })
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Quicksand', sans-serif;
+  list-style-type: none;
 }
 </style>
